a9d21ddf22ea62d48f69369d3cdd52eedd4942d4
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2022-08-23 16:44:54 +0900

    feat: flutter <-> webview 양방향 통신

--- lib/src/page/cbnu_alrami.dart
+++ lib/src/page/cbnu_alrami.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:cbnu_alrami_app/src/controller/notification_controller.dart';
+import 'package:get/get.dart';
+import 'dart:async';
+import 'dart:io';
 
-class CbnuAlramiWebview extends StatelessWidget {
-  const CbnuAlramiWebview({Key key}) : super(key: key);
+class CbnuAlramiWebview extends StatefulWidget {
+  CbnuAlramiWebview({Key key}) : super(key: key);
 
   @override
+  State<CbnuAlramiWebview> createState() => CbnuAlramiWebviewState();
+}
+class CbnuAlramiWebviewState extends State<CbnuAlramiWebview> {
+  WebViewController _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+  @override
   Widget build(BuildContext context) {
-    return const WebView(
-      initialUrl: 'https://dev-mobile.cmi.kro.kr/home', // 'https://dev-mobile.cmi.kro.kr'
+    final Completer<WebViewController> _controller =
+    Completer<WebViewController>();
+
+    NotificationController nc = new NotificationController();
+
+    return WebView(
+      initialUrl: 'http:10.0.2.2:3000/home', // 'https://dev-mobile.cmi.kro.kr'
       javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (WebViewController webviewController)  {
+        _controller.complete(webviewController);
+        _webViewController = webviewController;
+      },
+      javascriptChannels: <JavascriptChannel>{
+        _baseJavascript(context),
+      },
+      onPageFinished: (String url) async {
+        dynamic token = await nc.getToken();
+        _webViewController.evaluateJavascript('localStorage.setItem("token", "${token}");');
+      },
     );
+  }
+  JavascriptChannel _baseJavascript(BuildContext context) {
+    return JavascriptChannel(
+        name: 'baseApp',
+        onMessageReceived: (JavascriptMessage message) {
+            print(message.message);
+        });
   }
 }
